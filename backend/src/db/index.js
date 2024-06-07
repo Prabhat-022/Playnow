3371b1a43303628ef40d5a665ca6b5d3e74a8f19
Prabhat-022 <lograjanand1250@gmail.com>
2024-06-07 21:37:36 +0530

    Hii

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
 
-        console.log(connectionInstance);
+        // console.log(connectionInstance);
 
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance}`);
 
