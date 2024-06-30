feaad8dbbced3ccfb72741b6147f5e9a58bbecc7
Prabhat-022 <lograjanand1250@gmail.com>
2024-06-30 19:32:13 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 const corsOptions = {
-    origin:'http://localhost:3000',
+    origin:'https://playnow-by-pk.vercel.app',
     //for save the data on browser
     Credentials:true
 } 
